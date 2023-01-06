29dd90172aa14d050840ca7319986f9a2b9051ba
CannabiZz9 <hydrahuracan1@gmail.com>
2023-01-06 15:10:15 +0700

    Finish

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
-#include<string>
 #include<cstdlib>
+#include<string>
 #include<ctime>
 
 using namespace std;
@@ -9,53 +9,40 @@ string cardNames[] = {"","A","2","3","4","5","6","7","8","9","10","J","Q","K"};
 int cardScores[] = {0,1,2,3,4,5,6,7,8,9,10,10,10,10};
 
 int drawCard(void){
-	return rand()%13+1;
+    return rand()%13+1;
 }
 
 int calScore(int x,int y,int z){
-	//Write the function to calculate the score with is the rightmost digit of summation of the scores from three cards.
 	if(x>10) x = 10;
 	if(y>10) y = 10;
 	if(z>10) z = 10;
-	int score = x+y+z ;
-	return score%10 ;
+	int calscore = x+y+z ;
+	return calscore%10 ;
 }
 
 int findYugiAction(int s){	
-	if(s == 9) return 2; // Yugi will definitely stay (2) when current score (s) is equal to 9
-	else if(s < 6) return 1; // Yugi will definitely draw (1) when current score (s) is less than 6
+	if(s == 9) return 2;
+	else if(s < 6) return 1;
 	else{
-		// If current score is 6,7,8, Yugi will draw with probability 69% and will stay with probability 31% 
-		// Write conditions here using random number 
-		int yugiact = rand()%100+1;
-		if (yugiact <= 69){
-			return 1;
-		}
-		else{
-			return 2;
-		}
+		if (((rand()%100+1)>=70)and((rand()%100+1) <= 100)) return 2;
+		else return 1;
 	}
 }
 
 void checkWinner(int p, int y){
 	cout << "\n---------------------------------\n";
-	if ( p > y){
-		cout <<   "|         Player wins!!!        |"; // when p is greater than y
-	}
-	else if (p < y){
-		cout <<   "|          Yugi wins!!!         |"; // when p is less than y
-	}
-	else{
-		cout <<   "|             Draw!!!           |"; // when p is equal to y
-	}
+	if (p==y)     cout <<   "|             Draw!!!           |";
+	else if (p>y) cout <<   "|         Player wins!!!        |";
+	else if (p<y) cout <<   "|          Yugi wins!!!         |";
 	cout << "\n---------------------------------\n";
 }
 
 int main(){	
 	srand(time(0));
 	int playerScore, yugiScore, playerAction, yugiAction;
-	int playerCards[3] = {drawCard(),drawCard(),0};
-	int yugiCards[3]= {drawCard(),drawCard(),0};
+	int playerCards[3] = {drawCard(), drawCard(), 0};
+	int yugiCards[3] = {drawCard(), drawCard(), 0};
+	
 	
 	cout << "---------ORE NO TURN, DRAW!!!------------\n";
 	cout << "Your cards: " << cardNames[playerCards[0]] << " " << cardNames[playerCards[1]] << "\n";
@@ -64,7 +51,7 @@ int main(){
 	do{
 		cout << "(1) Destiny draw (2) Stay, SELECT: ";
 		cin >> playerAction;
-	}while(playerAction != 1 && playerAction != 2); 
+	}while((playerAction != 1) and (playerAction != 2));
 	if(playerAction == 1){
 		cout << "Player draws the 3rd card!!!" << "\n";
 		playerCards[2] = drawCard();
@@ -73,7 +60,6 @@ int main(){
 		cout << "Your new score: " << playerScore << "\n";
 	}
 	cout << "------------ Turn end -------------------\n\n";
-	
 	
 	cout << "---------YUGI'S TURN, DRAW!!!------------\n";
 	cout << "Yugi's cards: " << cardNames[yugiCards[0]] << " " << cardNames[yugiCards[1]] << "\n";
@@ -88,7 +74,7 @@ int main(){
 		cout << "Yugi's new score: " << yugiScore << "\n";
 	}
 	cout << "------------ Turn end -------------------\n";
-	
+
 	
 	checkWinner(playerScore,yugiScore);
 }
